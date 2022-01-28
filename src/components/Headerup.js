5151ebe63d961a954dfb264919333fd6dd1d8e30
arunetech <arun.etech11@gmail.com>
2022-01-28 18:34:45 +0530

    first-commit

---
+++ src/components/Headerup.js
@@ -0,0 +1,17 @@
+import react from "react";
+
+const Headerup = () => {
+  return (
+    <div className="container">
+      <div className="date">
+        <p>DATE - 20th August, 2021</p>
+      </div>
+      <div className="details">
+        <p>Privacy Policy</p>
+        <p>Teams and Conditions</p>
+        <p>Contact Us</p>
+      </div>
+    </div>
+  );
+};
+export default Headerup;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Headerup from "../components/Headerup";
+import styles from "../styles/HomePage.css"
+const HomePage = () => {
+  return <div>
+      <Headerup/>
+  </div>;
+};
+export default HomePage;
